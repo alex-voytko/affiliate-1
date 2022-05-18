231a61d2755e62b86bd7d6dcaaa2062e974df15a
alex-voytko <voytko1994@gmail.com>
2022-05-18 13:21:10 +0300

    edition 1.4 fix responsive

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,4 +1,11 @@
 new Swiper(".swiper", {
   slidesPerView: "auto",
   spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+  speed: 800,
+  grabCursor: true,
 });
